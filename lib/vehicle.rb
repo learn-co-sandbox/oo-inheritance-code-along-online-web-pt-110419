560481672fc475ecb98890419cdee3c4bec244e4
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-19 01:42:36 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,4 +3,13 @@ class Vehicle
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
+  end 
+  
+  def go 
+    "vrrrrrrrooom!"
+  end 
+  
+  def filling_up_tank
+    "filling up!"
+  end 
 end
